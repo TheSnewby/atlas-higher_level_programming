9e6dc4db0cf10bdd7e49c3028fc6804f343d6926
TheSnewby <stephen.j.newby@gmail.com>
2024-08-26 17:10:47 -0400

    modificed comb2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i > 0:
-        print(", ", end="")
-    print("{:0>{}}".format(i, 2), end="")
+    print((", " if i > 0 else "") + "{:0>{}}".format(i, 2), end="")
 print()
