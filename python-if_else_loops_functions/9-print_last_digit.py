07d3c0e2f1dbdce32659ea90963cc5a31e5f3506
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 16:39:36 -0500

    changed typecasting to %

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,5 @@ def print_last_digit(number):
     if number < 0:
         print('{}'.format(-1 * (abs(number) % 10)))
         return int(f'-{str(number)[-1]}')
-    print('{}'.format({str(number)[-1]}))
+    print('{}'.format(number % 10))
     return int(str(number)[-1])
