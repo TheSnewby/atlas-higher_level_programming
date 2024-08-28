69252623f30de02d161ed300cfe50e2675ffc619
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 16:35:53 -0500

    9. .format changes

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
+        print('{}'.format(-1 * (abs(number) % 10)))
         return int(f'-{str(number)[-1]}')
+    print('{}'.format({str(number)[-1]}))
     return int(str(number)[-1])
