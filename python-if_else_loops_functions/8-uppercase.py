0b19f3ea1d372898ddec5f97ee8a4da24fddd5fb
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 16:26:15 -0500

    8. changed sign

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+#Write a function that prints a string in uppercase followed by a new line.
 def uppercase(str):
     for i in str:
         if 97 <= ord(i) <= 123:
-            print('{}'.format(chr(ord(i + 32))), end="")
+            print('{}'.format(chr(ord(i)-32)), end="")
         else:
             print('{}'.format(i), end="")
+    print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
