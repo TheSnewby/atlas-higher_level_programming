88cb9d08730dc2ef3a03fb768e33911579e5e673
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 16:41:41 -0500

    removed negative sign

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        print('{}'.format(-1 * (abs(number) % 10)), end="")
-        return int(f'-{str(number)[-1]}')
+        print('{}'.format((abs(number) % 10)), end="")
+        return int(f'{str(number)[-1]}')
     print('{}'.format(number % 10), end="")
     return int(str(number)[-1])
