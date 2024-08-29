0b864b8239b0dbdc3ca98dd66715016f4e76fc2b
TheSnewby <stephen.j.newby@gmail.com>
2024-08-29 10:45:43 -0500

    add_tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_len = len(tuple_a)
+    b_len = len(tuple_b)
+    if a_len > 0:
+        first = tuple_a[0]
+    else:
+        first = 0
+    if b_len > 0:
+        first += tuple_b[0]
+
+    if a_len > 1:
+        second = tuple_a[1]
+    else:
+        second = 0
+    if b_len > 1:
+        second += tuple_b[1]
+    return (first, second)
