44d707ee6a6690cdeb56b0612de38a552dabe31b
TheSnewby <stephen.j.newby@gmail.com>
2024-08-29 10:20:22 -0500

    translate with dict

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = my_string.replace('c', '').replace('C', '')
+    new_string = my_string.translate({ord('c'): None, ord('C'): None})
     return new_string
