27bbea49b1228993de4bc01049b1f1b8b9518b41
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 21:08:34 -0500

    added plural clarity

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,12 @@
 import sys
 
 if __name__ == "__main__":
-    print('{} arguments'.format(len(sys.argv) - 1), end='')
-    print('{}'.format('.' if len(sys.argv) - 1 == 0 else ':'))
+    print('{} argument'.format(len(sys.argv) - 1), end='')
+    if len(sys.argv) - 1 == 0:
+        print('s.')
+    elif len(sys.argv) - 1 == 1:
+        print(':')
+    else:
+        print('s:')
     for i in range(1, len(sys.argv)):
         print('{}: {}'.format(i, sys.argv[i]))
