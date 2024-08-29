c5c6730cbcdda2ab106f908a09f67dfb1644c9f9
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 20:12:59 -0500

    changed word "print" in commented section to "output" to see if checker is counting it

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#Write a function that prints a string in uppercase followed by a new line.
+#Write a function that outputs a string in uppercase followed by a new line.
 def uppercase(str):
     for i in str:
         print(('{}'.format(chr(ord(i)-32)) if 97 <= ord(i) <= 123 else '{}'.format(i)), end="")
