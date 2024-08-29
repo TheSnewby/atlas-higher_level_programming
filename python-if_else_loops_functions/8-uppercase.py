5c601aa5d49330583f566c577a6ece1e9ad45a21
TheSnewby <stephen.j.newby@gmail.com>
2024-08-28 19:59:18 -0500

    used ternary to reduce number of print statements

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,5 @@
 #Write a function that prints a string in uppercase followed by a new line.
 def uppercase(str):
     for i in str:
-        if 97 <= ord(i) <= 123:
-            print('{}'.format(chr(ord(i)-32)), end="")
-        else:
-            print('{}'.format(i), end="")
+        print('{}'.format(chr(ord(i)-32)) if 97 <= ord(i) <= 123 else '{}'.format(i), end="")
     print()
