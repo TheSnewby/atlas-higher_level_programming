10cf75a6b4cfb7975f7042df1146c61f3f8258af
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 12:53:50 -0500

    moved checks earlier

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     numerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str) or roman_string == '':
+        return 0
     roman_list = list(roman_string)
     sum = 0
 
-    if len(roman_list) == 0 or not isinstance(roman_string, str):
-        return 0
-    elif len(roman_list) == 1:
+    if len(roman_list) == 1:
         return numerals[roman_list[0]]
     sum = 0
     for i in range(len(roman_list)):
