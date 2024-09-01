d57682d213671b35d9a6409f0e0d1f9b900055fe
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 11:19:41 -0500

    10-best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_value = list(a_dictionary.values())[0]
+    max_key = list(a_dictionary.keys())[0]
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return max_key
