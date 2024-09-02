3a3e9230d9460e28f387591ff12baed152d11ce1
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 19:14:39 -0500

    0 case

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     printed = 0
+    if x == 0:
+        return printed
     for item in my_list:
         try:
             print('{}'.format(item), end='')
