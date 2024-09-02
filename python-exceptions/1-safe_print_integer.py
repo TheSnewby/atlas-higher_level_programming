2bebac6c0b276d67f0f749c952eaf037285e006a
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 19:28:52 -0500

    1-safe

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    
+    try:
+        for num in value:
+            try:
+                print("{:d}".format(num))
+            except ValueError:
+                return False
+    except TypeError:
+        return False
+    return True
