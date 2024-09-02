29c3afe4cfab186e15cd9ed6d939f1380bcf7a4f
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 20:44:45 -0500

    added ValueError

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[]. x=0):
-    
+def safe_print_list_integers(my_list=[], x=0):
+    ints_printed = 0
+    if x == 0:
+        print()
+        return ints_printed
+    for item in my_list:
+        try:
+            print("{:d}".format(item), end='')
+            ints_printed += 1
+            if ints_printed == x:
+                break
+        except TypeError:
+            break
+        except ValueError:
+            continue
+    print()
+    return ints_printed
