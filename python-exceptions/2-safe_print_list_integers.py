4092ab3251cf46b4490f850d824820305833ac14
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 21:02:47 -0500

    changed for loop such that an indexerror could occur

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,15 +4,25 @@ def safe_print_list_integers(my_list=[], x=0):
     if x == 0:
         print()
         return ints_printed
-    for item in my_list:
+    for i in range(x):
         try:
-            print("{:d}".format(item), end='')
+            print("{:d}".format(my_list[i]), end='')
             ints_printed += 1
-            if ints_printed == x:
-                break
-        except TypeError:
-            break
-        except ValueError:
-            print("IndexError: list index out of range")
+        except (TypeError, ValueError):
+            continue
+        except IndexError:
+            raise IndexError("list index out of range")
     print()
     return ints_printed
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
