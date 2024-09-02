dcbe2203eeaa37fb2793393bc44b87d66e3d3d4c
TheSnewby <stephen.j.newby@gmail.com>
2024-09-01 19:14:03 -0500

    0-safe

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    for item in my_list:
+        try:
+            print('{}'.format(item), end='')
+        except IndexError:
+            break
+        printed += 1
+        if printed == x:
+            break
+    print()
+    return printed
