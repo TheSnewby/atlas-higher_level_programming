4702bf8c21edafb7b7ae2767bd6ad19bcc253a45
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 10:30:26 -0500

    0-

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module creates a Square class.
+"""
 class Square:
+    """
+    This class creates a Square class with one attribute.
+    """
     def __init__(self, side):
         self.side = side
