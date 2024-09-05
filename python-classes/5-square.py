da627b7865d28b2a857d07ab4370cab6208180c7
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 11:14:12 -0500

    len to range

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -38,7 +38,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in len(self.__size):
-                for j in len(self.__size):
+            for i in range(self.__size):
+                for j in range(self.__size):
                     print('#', end='')
                 print()
