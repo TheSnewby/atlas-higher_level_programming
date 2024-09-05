e72f84fe550cf1d6a858aea3bdde2f37e45aa6dc
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 11:13:39 -0500

    5-square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,44 @@
 #!/usr/bin/python3
+"""Printing a square of a Square class"""
+
+
+class Square:
+    """creates a Square class with options"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """returns area"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints the square with #s in stdout"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in len(self.__size):
+                for j in len(self.__size):
+                    print('#', end='')
+                print()
