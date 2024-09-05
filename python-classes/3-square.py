0c850d26f33c65b951ed8e93259c3cb3af4f8687
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 10:51:37 -0500

    3-square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Finds the area of a square
+"""
+
+
+class Square:
+    """
+    Creats a Square class with multiple options.
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise ValueError("size must be an integer")
+    def area(self):
+        return self.__size ** 2
