0c350b11e8598b5e56e740634880d153877c95e5
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 10:40:16 -0500

    1-square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+This module creates a square with size.
+"""
+
+
+class Square:
+    """
+    Square class with private size.
+    """
+    def __init__(self, size):
+        __size = size
