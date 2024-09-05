a1b215815fb7c88009455255bd3cdb2629530cca
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 10:47:36 -0500

    2-square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This module performs size validation on a Square class.
+"""
+
+
+class Square:
+    """
+    Class that creates a square with options.
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
