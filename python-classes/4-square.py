3e260605fc45b35b8a92748e1e1b9cec5256b3b9
TheSnewby <stephen.j.newby@gmail.com>
2024-09-05 11:00:04 -0500

    4-square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Access and updates private attribute of Square class"""
+
+
+class Square:
+    """creates a Square class with options"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    def size(self, value):
+        """sets the size"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """returns area"""
+        return self.__size ** 2
