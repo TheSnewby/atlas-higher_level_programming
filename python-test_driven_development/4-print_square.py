24d2db5029f9971b0532dde15e7b467abd7e41b2
TheSnewby <stephen.j.newby@gmail.com>
2024-09-09 16:11:25 -0500

    4-print_square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -3,4 +3,17 @@
 
 
 def print_square(size):
-    
+    """prints a square with the character #"""
+    if isinstance(size, int):
+        if size >= 0:
+            if not (size + 1 == size):
+                for i in range(size):
+                    for j in range(size):
+                        print('#', end='')
+                    print()
+            else:
+                raise ValueError("size is too large")
+        else:
+            raise ValueError("size must be >= 0")
+    else:
+        raise TypeError("size must be an integer")
