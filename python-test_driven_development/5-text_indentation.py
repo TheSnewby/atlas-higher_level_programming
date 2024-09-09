7dd37a011883b3a4fbaa0479c5e9653876b50451
TheSnewby <stephen.j.newby@gmail.com>
2024-09-09 16:55:46 -0500

    5-text

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Text indentation is text indentation"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters:
+    ., ?, and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    else:
+        new_string=''
+        for i in range(len(text)):
+            if text[i] != '!' and text[i] != ':' and text[i] != '.':
+                if i == len(text) - 1:
+                    new_string += text[i]
+                    print(new_string.strip())
+                else:
+                    new_string += text[i]
+            else:
+                new_string += text[i]
+                print(new_string.strip())
+                print()
+                new_string = ''
