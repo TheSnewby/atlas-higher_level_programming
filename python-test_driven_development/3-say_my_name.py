a0dc26e97de70fb86d05159ecadaf33043d12d5a
TheSnewby <stephen.j.newby@gmail.com>
2024-09-09 15:32:01 -0500

    txt file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,29 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """prints My name is <first name> <last name>
-    0) simple, successful case
-    >>> say_my_name('John', 'Adams') #0
-    My name is John Adams
-
-    1) parameters must be strings
-    >>> say_my_name(['John'], 'Adams') #1
-    Traceback (most recent call last):
-        ...
-    TypeError: first_name must be a string
-
-    2) parameters must be strings
-    >>> say_my_name('John', ['Adams']) #2
-    Traceback (most recent call last):
-        ...
-    TypeError: last_name must be a string
-
-    3) first_name mustn't be empty
-    >>> say_my_name() #3
-    Traceback (most recent call last):
-        ...
-    TypeError: say_my_name() missing 1 required positional argument: 'first_name'
-    """
+    """prints My name is <first name> <last name>"""
 
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
