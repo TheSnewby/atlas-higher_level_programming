6c32c35dbca6df4088ff7365d1a07a5798699ff7
TheSnewby <stephen.j.newby@gmail.com>
2024-09-09 11:16:20 -0500

    pycode, changed import syntax

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """add_integer returns the addition of two integers."""
+
+
 def add_integer(a, b=98):
     """Returns the addition of the integers a and b."""
     if not (isinstance(a, int) or isinstance(a, float)):
