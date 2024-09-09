f655850e7e47c6d02e619ceabc2daaeedc8edc22
TheSnewby <stephen.j.newby@gmail.com>
2024-09-09 11:10:15 -0500

    0-add_integer

--- python-test_driven_development/0-addinteger.py
+++ python-test_driven_development/0-addinteger.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""add_integer returns the addition of two integers."""
+def add_integer(a, b=98):
+    """Returns the addition of the integers a and b."""
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
