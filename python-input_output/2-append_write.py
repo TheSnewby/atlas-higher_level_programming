1adf14a7ae0d9bbce3b75a109262925f15fa36a8
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 17:32:34 -0500

    pycodestyle

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """Append to a file"""
 
 
-def append_write(filename='',text=''):
+def append_write(filename='', text=''):
     """appends a string at the end of a text file and returns chars added"""
     with open(filename, 'a') as f:
         chars_added = f.write(text)
