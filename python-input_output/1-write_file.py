6c842421ca7223647026b98de2b4bf82e213d4a2
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 17:22:30 -0500

    1-write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""write to a file"""
+
+
+def write_file(filename='', text=''):
+    """writes a string to a textfile and returns the num of chars written"""
+    with open(filename, 'w+') as f:
+        f.write(text)
