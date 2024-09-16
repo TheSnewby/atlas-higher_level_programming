5f0a83873efabb7e34e60519d79c80e44bec5282
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 01:11:49 -0500

    print sorted for an empty list prints []

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,6 +6,6 @@ class MyList(list):
     """Class inherits from list."""
     def print_sorted(self):
         """prints a sorted (ascending) list."""
-        if isinstance(self, list) and self:
+        if isinstance(self, list):
             new_list = self[:]
             print(sorted(new_list))
