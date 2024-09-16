a4304f81d0f06d95f4f3c1501fc4da44da5ea0fb
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 17:10:09 -0500

    adjust print message

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """reads a text file and prints it to stdout"""
     with open(filename, 'r') as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end='')
     f.closed
