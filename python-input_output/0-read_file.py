8fae1c02c905f1e6718de73d95bfedaef884bd97
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 17:08:56 -0500

    adjusted open

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,7 +4,7 @@
 
 def read_file(filename=""):
     """reads a text file and prints it to stdout"""
-    with open('filename', encoding="utf-8") as f:
+    with open('filename', 'r') as f:
         read_data = f.read()
         print(read_data)
     f.closed
