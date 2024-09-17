a35bba8c94819d6ad755075ab485d18cda814a49
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 15:29:53 -0500

    removed .py from imports

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-Module add_item
 Load, add, save
 """
 
 import sys
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 p_list = sys.argv[1:]
 try:
