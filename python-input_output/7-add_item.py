b882cf4486a3277466d3e6ba5d432037b8970942
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 15:26:14 -0500

    documentation, added try-except

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Load, add, save"""
+"""
+Load, add, save
+add_item
+"""
 import json
 import sys
 save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
-
 p_list = sys.argv[1:]
-load_from_json_file('add_item.json')
-for i in range(len(p_list)):
-    # consider if not created, create it
-    save_to_json_file(p_list[i], 'add_item.json')
+try:
+    f = load_from_json_file('add_item.json')
+except Exception:
+    f = []
 
+save_to_json_file(p_list, 'add_item.json')
