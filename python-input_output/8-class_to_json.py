23fa7c08cf00fe8cab0dd0f53b20d80034a00997
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 15:58:03 -0500

    pep8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,5 +4,5 @@
 
 
 def class_to_json(obj):
-    """returns the dictionary description with simple data structure for JSON"""
+    """returns the dictionary description for JSON"""
     return obj.__dict__
