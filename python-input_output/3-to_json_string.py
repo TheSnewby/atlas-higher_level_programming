86c132c3e8731a92041b6a2f1fd1ae48eb084803
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 22:57:15 -0500

    json dumps

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """To JSON String"""
+import json
 
 
 def to_json_string(my_obj):
     """returns the JSON reprepsentation of an object (string)"""
-    return str(my_obj)
+    return json.dumps(my_obj)
