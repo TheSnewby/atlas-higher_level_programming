287f29bc473677da530b10cf92a8c274445f8f67
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 15:43:14 -0500

    changed 5-save to have append permission

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using JSON representation"""
-    with open(filename, 'w') as f:
+    with open(filename, 'a') as f:
         f.write(json.dumps(my_obj))
