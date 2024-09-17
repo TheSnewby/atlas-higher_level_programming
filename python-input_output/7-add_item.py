77b6e2b856cfeeb4029b4e9115c0dde4867231fe
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 15:56:15 -0500

    extended loaded list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,10 +7,10 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-p_list = sys.argv[1:]
+arg_list = sys.argv[1:]
 try:
-    f = load_from_json_file('add_item.json')
+    file_list = load_from_json_file('add_item.json')
 except Exception:
-    f = []
-
-save_to_json_file(p_list, 'add_item.json')
+    file_list = []
+file_list.extend(arg_list)
+save_to_json_file(file_list, 'add_item.json')
