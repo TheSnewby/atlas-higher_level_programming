490c048b894e408eebc0ac981e3c05a5491caac9
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 18:16:30 -0500

    12-pascal

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+    """creates a pascal's triangle n-levels tall"""
+    if n <= 0:
+        return []
+    p = [[]]
+    for r in range(1, n + 1): # r is row
+        for c in range(r): # c is column
+            p.append([])
+            if c == 0:
+                p[r].append(1)
+            elif c == r - 1:
+                p[r].append(1)
+            else:
+                p[r].append(p[r - 1][c - 1] + p[r - 1][c])
+        print(p[r])
