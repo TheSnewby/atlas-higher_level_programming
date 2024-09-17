eac1c1e4a671ab8c86aabb3fdc4cb70b2eae70e5
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 18:53:59 -0500

    pep8

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,9 +7,9 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     p = []
-    for r in range(0, n): # r is row
+    for r in range(0, n):  # r is row
         p.append([])
-        for c in range(r+1): # c is column
+        for c in range(r+1):  # c is column
             if c == 0:
                 p[r].append(1)
             elif c == r:
