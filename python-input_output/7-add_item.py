d1f749dfe990984820fc9091a6bfad3d73c22480
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 15:17:54 -0500

    7-add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Load, add, save"""
+import json
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+
+p_list = sys.argv[1:]
+load_from_json_file('add_item.json')
+for i in range(len(p_list)):
+    # consider if not created, create it
+    save_to_json_file(p_list[i], 'add_item.json')
+
