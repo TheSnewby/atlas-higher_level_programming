cc6a91b111ba5140ad347796ea5ec7b811dfead9
TheSnewby <stephen.j.newby@gmail.com>
2024-09-16 23:20:52 -0500

    load from json file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Create object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Ojbect from a JSON file"""
+    with open(filename, 'r') as f:
+        return json.load(f)
