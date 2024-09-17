5749d86cb77ea359587279a14aa43d70af3d5e17
TheSnewby <stephen.j.newby@gmail.com>
2024-09-17 18:52:34 -0500

    changes ranges and the elif check

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,14 +6,17 @@ def pascal_triangle(n):
     """creates a pascal's triangle n-levels tall"""
     if n <= 0:
         return []
-    p = [[]]
-    for r in range(1, n + 1): # r is row
-        for c in range(r): # c is column
-            p.append([])
+    p = []
+    for r in range(0, n): # r is row
+        p.append([])
+        for c in range(r+1): # c is column
             if c == 0:
                 p[r].append(1)
-            elif c == r - 1:
+            elif c == r:
                 p[r].append(1)
             else:
                 p[r].append(p[r - 1][c - 1] + p[r - 1][c])
     return p
+result = pascal_triangle(5)
+for row in result:
+    print(row)
