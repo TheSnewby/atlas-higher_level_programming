eca7eb86ba1d2a8ba4d9fd6ce18f7454a9b1661e
TheSnewby <stephen.j.newby@gmail.com>
2024-09-20 00:08:56 -0700

    documented base functions

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initializer of Base instances"""
         if id is not None:
             self.id = id
         else:
@@ -16,11 +17,13 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """converts list of dictionaries to a json string"""
         if list_dictionaries is None:
             return '[]'
         return json.dumps(list_dictionaries)
 
     def save_to_file(cls, list_objs):
+        """saves a list of dictionaries to a file"""
         Rectangle = __import__('rectangle').Rectangle
         if isinstance(cls, Rectangle):
             with open('Rectangle.json', 'w') as f:
@@ -30,11 +33,13 @@ class Base:
                 f.write(Base.to_json_string(list_objs))
 
     def from_json_string(json_string):
+        """unpacks a list of dictionaries from a json string"""
         if json_string is None:  # how to do if empty
             return []
         return json.loads(json_string)
 
     def create(cls, **dictionary):
+        """creates new class instances from list of dictionaries"""
         Rectangle = __import__('rectangle').Rectangle
         Square = __import__('square').Square
         if isinstance(cls, Rectangle):
@@ -47,6 +52,7 @@ class Base:
             return new_square
 
     def load_from_file(cls):
+        """loads a class instance from a file"""
         Rectangle = __import__('rectangle').Rectangle
         if cls is None:
             return []
