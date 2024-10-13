caedf670e01389824e995e070c66c2c41e0b1dee
TheSnewby <stephen.j.newby@gmail.com>
2024-10-12 22:56:59 -0500

    added procedure

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,13 @@
 -- a script that creates the MySQL server user user_0d_1.
-SELECT COUNT(*) INTO existance FROM mysql.user WHERE user = 'user_0d_1' and host = 'localhost';
-IF existance = 0 THEN
-    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-    GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
-END IF
+DELIMITER //CREATE PROCEDURE createIfNonexistent()
+BEGIN
+    DECLARE existence INT;
+    SELECT COUNT(*) INTO existance FROM mysql.user WHERE user = 'user_0d_1' and host = 'localhost';
+    IF existance = 0 THEN
+        CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+        GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
+    END IF
+END
+DELIMITER;
+
+CALL createIfNonexistent();
