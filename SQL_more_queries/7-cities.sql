3f83ea0431d1bfe293a49a46705e7e6dfc9dd7f6
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 00:29:35 -0500

    corrected foreign key syntax

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -10,5 +10,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    FOREIGN KEY(state_id INT NOT NULL) REFERENCES (hbtn_0d_usa.states),
-    name VARCHAR(256) NOT NULL);
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id));
