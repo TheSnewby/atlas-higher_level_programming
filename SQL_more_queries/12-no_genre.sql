9ed48d3abee758ecd7c500f6249efae77f8a2726
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 13:37:30 -0500

    fixed left join and where

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -6,6 +6,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres.genre_id ON tv_shows.title = tv_show_genres.genre_id
-WHERE tv_show_genres.genre_id = NULL
+LEFT JOIN tv_show_genres.genre_id ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
