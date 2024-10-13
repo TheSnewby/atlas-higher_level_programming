cbc07b5f10ef1bebee01d27cfef001f057731c5e
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 00:21:50 -0500

    changed DEFAULT to UNIQUE, added USE new database, changed auto generate to auto_increment

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -7,4 +7,6 @@
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT AUTO GENERATED DEFAULT NOT NULL PRIMARY KEY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, 
+    name VARCHAR(256));
