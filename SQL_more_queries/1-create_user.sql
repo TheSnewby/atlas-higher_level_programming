22bcdabebb4d487ea909517307a8484e061934a5
TheSnewby <stephen.j.newby@gmail.com>
2024-10-12 22:44:02 -0500

    added if statement

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- a script that creates the MySQL server user user_0d_1.
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
+SELECT COUNT(*) INTO existance FROM mysql.user WHERE user = 'user_0d_1' and host = 'localhost';
+IF existance = 0 THEN
+    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
+END IF
