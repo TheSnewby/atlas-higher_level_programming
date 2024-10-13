8c221108678946086d469eb6d2885d00324ca381
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 11:44:19 -0500

    caps

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,5 +5,5 @@
 -- You are not allowed to use the JOIN keyword
 -- The database name will be passed as an argument of the mysql command
 SELECT id, name IN hbtn_0d_usa.cities 
-WHERE state_id = (SELECT id from hbtn_0d_usa.states WHERE name = 'California')
+WHERE state_id = (SELECT id FROM hbtn_0d_usa.states WHERE name = 'California')
 ORDER BY id ASC;
