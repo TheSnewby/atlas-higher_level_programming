6e3ad2482acda20bd1d472845e6682a6e8a5fe9a
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 00:15:30 -0500

    added use

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,4 +6,5 @@
 -- If the database hbtn_0d_usa already exists, your script should not fail
 -- If the table states already exists, your script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT AUTO GENERATED DEFAULT NOT NULL PRIMARY KEY, name VARCHAR(256));
