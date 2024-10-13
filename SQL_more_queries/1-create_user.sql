6836ea1d2ca5d08b9afc2e2c2f8618d38a1282c5
TheSnewby <stephen.j.newby@gmail.com>
2024-10-12 22:59:09 -0500

    typo and delimeters

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,13 +1,14 @@
 -- a script that creates the MySQL server user user_0d_1.
-DELIMITER //CREATE PROCEDURE createIfNonexistent()
+DELIMITER //
+CREATE PROCEDURE createIfNonexistent()
 BEGIN
-    DECLARE existence INT;
+    DECLARE existance INT;
     SELECT COUNT(*) INTO existance FROM mysql.user WHERE user = 'user_0d_1' and host = 'localhost';
     IF existance = 0 THEN
         CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
         GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
     END IF
-END
-DELIMITER;
+END //
+DELIMITER ;
 
 CALL createIfNonexistent();
