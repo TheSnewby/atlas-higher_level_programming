e6b4f22f3dd3c8d1b2cf45a469902e19d9d58dd7
TheSnewby <stephen.j.newby@gmail.com>
2024-10-12 23:42:26 -0500

    removed procedure

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,14 +1,16 @@
 -- a script that creates the MySQL server user user_0d_1.
-DELIMITER //
-CREATE PROCEDURE createIfNonexistent()
-BEGIN
-    DECLARE existence INT DEFAULT 0;
-    SELECT COUNT(*) INTO existence FROM mysql.user WHERE user = 'user_0d_1' and host = 'localhost';
-    IF existence = 0 THEN
-        CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-    END IF;
-    GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
-END //
-DELIMITER ;
+-- DELIMITER //
+-- CREATE PROCEDURE createIfNonexistent()
+-- BEGIN
+--     DECLARE existence INT DEFAULT 0;
+--     SELECT COUNT(*) INTO existence FROM mysql.user WHERE user = 'user_0d_1' and host = 'localhost';
+--     IF existence = 0 THEN
+--         CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+--     END IF;
+--     GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
+-- END //
+-- DELIMITER ;
 
-CALL createIfNonexistent();
+-- CALL createIfNonexistent();
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
