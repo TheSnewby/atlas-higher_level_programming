e1507c4a9c5f9c36e114b43a3d48c22c935d539d
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 20:43:40 -0500

    added GROUP BY and WHERE

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -10,4 +10,6 @@
 SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+WHERE COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
