445be6c23da18b660a781f0a30f7b94fa323468b
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 20:53:38 -0500

    mistyped table name

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,9 +7,9 @@
 -- Results must be sorted in descending order by the number of shows linked
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
 INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
+GROUP BY tv_genres.name
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
