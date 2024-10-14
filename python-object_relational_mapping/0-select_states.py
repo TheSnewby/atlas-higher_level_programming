f38cfa4c414ec523cc9009c3e884714fb33c1d14
TheSnewby <stephen.j.newby@gmail.com>
2024-10-14 13:56:51 -0500

    added info

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id FROM {} ORDER BY id ASC".format(db))
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
