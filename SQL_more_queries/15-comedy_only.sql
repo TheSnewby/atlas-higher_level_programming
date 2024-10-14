3b00a689e7aad25ced4967a700b199d1ba1d746a
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 22:13:05 -0500

    typo

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -8,6 +8,6 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title FROM tv_shows
-INNER JOIN tv_show_genres ON tv-shows.id = tv_show_genres.show_id
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC;
