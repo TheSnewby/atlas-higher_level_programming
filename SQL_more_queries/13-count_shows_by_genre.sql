0620fc0e0a1746d4a40b7743b2de81c372664f68
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 20:54:13 -0500

    another mistype

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,7 +9,7 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
