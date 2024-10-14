0d9c5022740e1883f6d4122d09cf19120895f58f
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 22:19:30 -0500

    removed distinct added where

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -7,7 +7,8 @@
 -- Results must be sorted in ascending order by the show title
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT DISTINCT tv_shows.title FROM tv_shows
+SELECT tv_shows.title FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
