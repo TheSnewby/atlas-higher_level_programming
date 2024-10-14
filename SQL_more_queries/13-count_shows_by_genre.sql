66a3b4fd1f7f04cfe9de82e9b53ba36ee12819d1
TheSnewby <stephen.j.newby@gmail.com>
2024-10-13 20:44:24 -0500

    changed WHERE to HAVING

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -11,5 +11,5 @@ SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 GROUP BY genres.name
-WHERE COUNT(tv_show_genres.show_id) > 0
+HAVING COUNT(tv_show_genres.show_id) > 0
 ORDER BY number_of_shows DESC;
