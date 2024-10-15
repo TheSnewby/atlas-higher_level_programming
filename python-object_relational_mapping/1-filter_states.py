ffd485626b25d6d526e1c4e89d1554c4710de1b0
TheSnewby <stephen.j.newby@gmail.com>
2024-10-15 12:24:25 -0500

    1-filter

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+import re
+
+
+if __name__ == "__main__":
+    """lists all states with a name starting with N (upper N)."""
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if re.search('d N*'):
+            print(row)
+    cur.close()
+    db.close()
