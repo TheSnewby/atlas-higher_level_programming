2fc45f0b1cca9d734f5d8d0fc6c8f59522ab5712
TheSnewby <stephen.j.newby@gmail.com>
2024-10-15 13:12:28 -0500

    removed comma

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
-                FROM cities, \
+                FROM cities \
                 INNER JOIN cities.state_id = states.id \
                 ORDER BY cities.id ASC")
     rows = cur.fetchall()
