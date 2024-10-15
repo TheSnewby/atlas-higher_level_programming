68aa1a203c15413cd2b120c971c030efdf58b4d6
TheSnewby <stephen.j.newby@gmail.com>
2024-10-15 13:14:46 -0500

    fixed inner join by added ON

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name \
                 FROM cities \
-                INNER JOIN cities.state_id = states.id \
+                INNER JOIN states ON cities.state_id = states.id \
                 ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
