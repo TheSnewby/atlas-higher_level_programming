d448e4a85cf6a58be08c1f8e4d5e6d8222d29f7c
TheSnewby <stephen.j.newby@gmail.com>
2024-10-15 17:38:59 -0500

    name main move

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,22 +9,20 @@ from model_state import Base, State
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-mysql_database = sys.argv[3]
-
-url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(mysql_username,
-                                                       mysql_password,
-                                                       mysql_database)
-engine = create_engine(url)
-connection = engine.connect()
-query_result = connection.execute("SELECT * FROM states")
-rows = query_result.fetchall()
-for row in rows:
-    print(row)
-
-
 if __name__ == "__main__":
     """main function"""
-    pass
+
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_database = sys.argv[3]
+
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(mysql_username,
+                                                        mysql_password,
+                                                        mysql_database)
+    engine = create_engine(url)
+    connection = engine.connect()
+    query_result = connection.execute("SELECT * FROM states")
+    rows = query_result.fetchall()
+    for row in rows:
+        print(row)
