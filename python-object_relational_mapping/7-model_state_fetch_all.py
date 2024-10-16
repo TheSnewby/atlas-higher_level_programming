6fad8f98eac8e5409b924cef7880858f26a65311
TheSnewby <stephen.j.newby@gmail.com>
2024-10-15 19:23:16 -0500

    undid for loop change

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -27,5 +27,5 @@ if __name__ == "__main__":
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
-        print("{}: {}".format(states.id, states.name))
+        print("{}: {}".format(state.id, state.name))
     session.close()
