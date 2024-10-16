7f82de83b4d838640227cbaed7c7498d89479bff
TheSnewby <stephen.j.newby@gmail.com>
2024-10-15 21:32:52 -0500

    pycodestlye

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Sesh = sessionmaker(bind=engine)
     session = Sesh()
-    state = session.query(State).filter_by(id = 2).first()
+    state = session.query(State).filter_by(id=2).first()
     if state:
         state.name = 'New Mexico'
     session.commit()
