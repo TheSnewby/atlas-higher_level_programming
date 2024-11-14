814e65a4675814e868a1a1aef1ad2e0cf645ed44
TheSnewby <stephen.j.newby@gmail.com>
2024-11-14 11:49:20 -0600

    removed autoadded require

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-const { isNumber } = require('util');
 
 if (argv[2] === undefined || isNaN(Number(argv[2]))) {
   console.log('Missing size');
