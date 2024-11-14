a2afe51c2486044f01f6d6057a6aab0568d4777f
TheSnewby <stephen.j.newby@gmail.com>
2024-11-14 11:48:35 -0600

    changed conditional

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
+const { isNumber } = require('util');
 
-if (argv[2] === undefined) {
+if (argv[2] === undefined || isNaN(Number(argv[2]))) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < argv[2]; i++) {
