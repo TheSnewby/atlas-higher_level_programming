093418caae16f98596cd9da819e2b28e56135c88
TheSnewby <stephen.j.newby@gmail.com>
2024-11-15 11:10:40 -0600

    7-occur

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-const Square = require('./6-square');
 
-const s1 = new Square(4);
-s1.charPrint();
-
-s1.charPrint('C');
+exports.nb0ccurences = function (list, searchElement) {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
