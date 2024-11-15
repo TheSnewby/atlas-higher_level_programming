ad242ef1206090a71eddac30efe2758a1764f6a8
TheSnewby <stephen.j.newby@gmail.com>
2024-11-15 15:49:18 -0600

    used for loop instead

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const reverse = list.slice().reverse();
+  const reverse = [];
+  for (let i = 0; i < list.length; i++) {
+    reverse[i] = list[list.length - 1 - i];
+  }
   return reverse;
 };
