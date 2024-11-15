34d630dff34b2c5084562f7d5bfdf6de311dd462
TheSnewby <stephen.j.newby@gmail.com>
2024-11-15 10:30:00 -0600

    changed this.size to this.height

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,8 @@
 const fiveSquare = require('./5-square.js');
 module.exports = class Square extends fiveSquare {
   charPrint (c) {
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.weight; j++) {
         if (c === undefined) {
           process.stdout.write('X');
         } else {
