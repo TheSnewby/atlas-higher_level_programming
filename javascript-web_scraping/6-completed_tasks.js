d124b4026410b3432fd7ffa52ef6e32252872def
TheSnewby <stephen.j.newby@gmail.com>
2024-11-23 17:02:07 -0600

    6-completed

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const request = require('request');
+let results = {}
+
+request(argv[2], (err, response, body) => {
+  if (err) throw err;
+  else {  // else statement not needed because throw skips scope
+    body = JSON.parse(body);
+    for (const element of body) {
+      if (element.completed) {
+        if (element.userId in results) {
+            results[element.userId] += 1;
+        } else {
+            results[element.userId] = 1;
+        }
+      }
+    }
+    console.log(results);
+  }
+});
