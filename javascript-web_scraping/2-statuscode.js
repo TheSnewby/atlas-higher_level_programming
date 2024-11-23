42d7765d7e86d9bd0664c3b337c4d0e4f29dfd27
TheSnewby <stephen.j.newby@gmail.com>
2024-11-23 14:16:00 -0600

    forgot specific output

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const request = require('request');
 
 request(argv[2], (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    console.log(body);
+    console.log(`code:  ${response.statusCode}`);
   }
 });
