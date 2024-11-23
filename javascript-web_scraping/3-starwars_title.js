370d356e851e3b65608c85114acdded3bd58ba4a
TheSnewby <stephen.j.newby@gmail.com>
2024-11-23 14:29:35 -0600

    .title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -8,6 +8,6 @@ const url = 'https://swapi-api.hbtn.io/api/films/';
 request(url + argv[2], (err, response, body) => {
   if (err) throw err;
   if (!err && response.statusCode === 200) {
-    console.log(JSON.parse(body)['title']);
+    console.log(JSON.parse(body).title);
   }
 });
